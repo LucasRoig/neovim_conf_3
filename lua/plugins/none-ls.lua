acfb6d14bc6b83d1ed9819f626fc0d0ac4ebdc56
Lucas Roig <lucas.roig1@gmail.com>
2024-05-10 21:57:01 +0200

    add copilot

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,23 +1,22 @@
-local formatters = {
-    "stylua", "prettierd"
-}
+local formatters = { "stylua", "prettierd" }
 
-return  {
+return {
     "jay-babu/mason-null-ls.nvim",
     dependencies = {
         "nvimtools/none-ls.nvim",
     },
     config = function()
         require("mason-null-ls").setup({
-            ensure_installed = formatters
+            ensure_installed = formatters,
         })
-        
+
         local null_ls = require("null-ls")
         null_ls.setup({
             sources = {
-                null_ls.builtins.formatting.stylua,  
-                null_ls.builtins.formatting.prettierd
-            }
+                null_ls.builtins.formatting.stylua,
+                null_ls.builtins.formatting.prettierd,
+            },
         })
-    end
+    end,
 }
+
