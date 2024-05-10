3a91f8abb0fe64e1fa240e462c552c966be6cd24
Lucas Roig <lucas.roig1@gmail.com>
2024-05-10 18:32:37 +0200

    add nvim-tmux-navigator

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -5,3 +5,9 @@ vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.g.mapleader = " "
 vim.wo.relativenumber = true
+
+-- Pane navigation
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
