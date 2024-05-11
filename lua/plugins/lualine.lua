5e5390ca608bfe7e47cace822148e4b021c06c0f
Lucas Roig <lucas.roig1@gmail.com>
2024-05-11 18:40:07 +0200

    update lualine to show full filepath

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,11 +1,14 @@
 return {
-    'nvim-lualine/lualine.nvim',
-    dependencies = {'nvim-tree/nvim-web-devicons'},
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-        require('lualine').setup {
+        require("lualine").setup({
             options = {
-                theme = 'catppuccin'
-            }
-        }
-    end
+                theme = "catppuccin",
+            },
+            sections = {
+                lualine_c = { { "filename", path = 1 } },
+            },
+        })
+    end,
 }
