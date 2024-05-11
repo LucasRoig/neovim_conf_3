8c7079e22bda8f5746392002734f4fd63aa4bdad
Lucas Roig <lucas.roig1@gmail.com>
2024-05-11 17:21:38 +0200

    add keymap for saving buffer

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -11,3 +11,6 @@ vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
 vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+-- Save file
+vim.keymap.set('n', '<leader>w', ':w<CR>', { desc = "Save buffer"})
